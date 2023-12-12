ce7d335dea8ad079e14d10bd6740471b179d0b2d
Jym-lab <jym9809@gmail.com>
2023-12-12 16:43:58 +0900

    feat: Course JDBC

---
+++ 2.learn-jpa-and-hibernate/src/main/java/com/minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -0,0 +1,13 @@
+package com.minutes.springboot.learnjpaandhibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+    @Autowired
+    private CourseJdbcRepository repository;
+    @Override
+    public void run(String... args) throws Exception {
+        repository.insert();
+    }
+}

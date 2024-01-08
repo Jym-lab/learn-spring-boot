02efe1ff0059af4764295c7000a0a3af993e995c
Jym-lab <jym9809@gmail.com>
2024-01-08 11:08:16 +0900

    feat: User created

--- 4.restful-web-services/src/main/java/com/yjoo/rest/webservices/restfulwebservices/user/UserResource.java
+++ 4.restful-web-services/src/main/java/com/yjoo/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,9 +1,10 @@
 package com.yjoo.rest.webservices.restfulwebservices.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,5 +23,12 @@ public class UserResource {
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
         return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@RequestBody User user){
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location ).build();
     }
 }
